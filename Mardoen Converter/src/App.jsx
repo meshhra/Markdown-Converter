024e5ff35f32ea3ebb977df649dffbdc17d878f2
Divyansh Mishra <88994556+meshhra@users.noreply.github.com>
2024-03-15 21:15:20 +0530

    Over

--- Mardoen Converter/src/App.jsx
+++ Mardoen Converter/src/App.jsx
@@ -45,7 +45,7 @@ And here. | Okay. | I think we get it.
 1. Use just 1s if you want!
 1. And last but not least, let's not forget embedded images:
 
-![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)`
+![Desert Landscape](https://images.unsplash.com/photo-1707343844152-6d33a0bb32c3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`;
 
 function App() {
   const [editorValue, setEditorValue] = useState(defaultText);
@@ -56,6 +56,7 @@ function App() {
       </header>
       <main>
         <Editor editorValue={editorValue} setEditorValue={setEditorValue} />
+        <div className="divide"></div>
         <Preview editorValue={editorValue} />
       </main>
     </>
@@ -65,10 +66,12 @@ function App() {
 function Editor({ editorValue, setEditorValue }) {
   return (
     <section id="editor-section">
+      <div className="section-title"><h4>Editor</h4></div>
       <textarea
         name="editor"
         id="editor"
         value={editorValue}
+        spellCheck={false}
         onChange={(e) => setEditorValue(e.target.value)}
       ></textarea>
     </section>
@@ -77,9 +80,12 @@ function Editor({ editorValue, setEditorValue }) {
 
 function Preview({ editorValue }) {
   const gfmRenderer = remarkGfm;
-  const clean = <ReactMarkdown remarkPlugins={[gfmRenderer]}>{editorValue}</ReactMarkdown>;
+  const clean = (
+    <ReactMarkdown remarkPlugins={[gfmRenderer]}>{editorValue}</ReactMarkdown>
+  );
   return (
     <section id="preview-section">
+      <div className="section-title"><h4>preview</h4></div>
       <div id="preview">{clean}</div>
     </section>
   );
