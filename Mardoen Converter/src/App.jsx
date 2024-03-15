88ca4f90b63554be0c380aa6815beb005b009573
Divyansh Mishra <88994556+meshhra@users.noreply.github.com>
2024-03-15 18:50:57 +0530

    Converts markdown to sanatized html

--- Mardoen Converter/src/App.jsx
+++ Mardoen Converter/src/App.jsx
@@ -1,5 +1,38 @@
+import { useState } from "react";
+import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
+import DOMPurify from 'isomorphic-dompurify';
 function App() {
-  return <div>App</div>;
+  const [editorValue, setEditorValue] = useState("");
+  return (
+    <>
+      <header>
+        {/* this will contain options reguarding file saving and tiher things */}
+      </header>
+      <main>
+        <Editor editorValue={editorValue} setEditorValue={setEditorValue} />
+        <Preview editorValue={editorValue} />
+      </main>
+    </>
+  );
+}
+
+function Editor({ editorValue, setEditorValue }) {
+  return (
+    <section id="editor-section">
+      <textarea
+        name="editor"
+        id="editor"
+        value={editorValue}
+        onChange={(e) => setEditorValue(e.target.value)}
+      ></textarea>
+    </section>
+  );
+}
+
+function Preview({editorValue}) {
+  return <section id="preview-section">
+    <div id="preview">{DOMPurify.sanitize(marked.parse(editorValue))}</div>
+  </section>
 }
 
 export default App;
